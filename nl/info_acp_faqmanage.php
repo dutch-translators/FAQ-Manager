d69b1a64a95b164340b4822e41f343847e50e5d3
David <neurofibromatose@live.nl>
2015-01-15 15:26:18 +0100

    beginnetje + credit line
    De eerste variabelen heb ik alvast vertaald, nog steeds bezig...

--- nl/info_acp_faqmanage.php
+++ nl/info_acp_faqmanage.php
@@ -4,6 +4,7 @@
 * @package FAQ Manager
 * @copyright (c) 2015 david63
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* [Dutch] translated by Dutch Translators (https://github.com/dutch-translators)
 *
 */
 
@@ -34,28 +35,28 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'BACKUP_LOCATION_NO_WRITE'	=> 'Unable to create a backup file. Please check the directory permissions for store/faq_backup/ and every directory and file in it.',
-	'BAD_FAQ_FILE'				=> 'The file you are attempting to edit is not a FAQ file.',
+	'BAD_FAQ_FILE'				=> 'Het bestand dat je probeert te bewerken is geen FAQ-bestand',
 
-	'CREATE_CATEGORY'			=> 'Create category',
-	'CREATE_FIELD'				=> 'Create topic',
+	'CREATE_CATEGORY'			=> 'Creëer categorie ',
+	'CREATE_FIELD'				=> 'Creëer onderwerp ',
 
-	'DELETE_CAT'				=> 'Delete category',
-	'DELETE_VAR'				=> 'Delete topic',
+	'DELETE_CAT'				=> 'Verwijder categorie',
+	'DELETE_VAR'				=> 'Verwijder onderwerp',
 
-	'FAQ_CAT_LIST'				=> 'Here you can see and edit existing Categories.<br /><br /><strong>Note that a category named -- is the section break for the FAQ display page.</strong>',
-	'FAQ_EDIT_SUCCESS'			=> 'The FAQ was updated successfully.',
-	'FAQ_FILE_NOT_EXIST'		=> 'The file you are attempting to edit does not exist.',
+	'FAQ_CAT_LIST'				=> 'Hier kan je bestaande categorieën zien en wijzigen.<br /><br /><strong>Note that a category named -- is the section break for the FAQ display page.</strong>',
+	'FAQ_EDIT_SUCCESS'			=> 'De FAQ is succesvol bijgewerkt.',
+	'FAQ_FILE_NOT_EXIST'		=> 'Het bestand dat je probeert te wijzigen bestaat niet.',
 	'FAQ_FILE_NO_WRITE'			=> 'Unable to update the file.  Please check the file permissions for the file you are attempting to edit.',
-	'FAQ_FILE_SELECT'			=> 'Select the file you would like to edit.',
-	'FAQ_MANAGER'				=> 'FAQ Manager',
-	'FAQ_MANAGE'				=> 'Manage FAQs',
+	'FAQ_FILE_SELECT'			=> 'Selecteer het bestand dat je wilt wijzigen.',
+	'FAQ_MANAGER'				=> 'FAQ Beheerder',
+	'FAQ_MANAGE'				=> 'Beheer FAQs',
 
-	'LOAD_BACKUP'				=> 'Load Backup',
+	'LOAD_BACKUP'				=> 'Backup laden',
 
-	'LOG_FAQ_ADD'				=> '<strong>FAQ item added</strong>',
-	'LOG_FAQ_DELETE'			=> '<strong>FAQ item deleted</strong>',
-	'LOG_FAQ_EDIT'				=> '<strong>FAQ item edited</strong>',
-	'LOG_FAQ_RESTORE'			=> '<strong>FAQ restored from backup</strong>',
+	'LOG_FAQ_ADD'				=> '<strong>FAQ item toegevoegd</strong>',
+	'LOG_FAQ_DELETE'			=> '<strong>FAQ item verwijderd</strong>',
+	'LOG_FAQ_EDIT'				=> '<strong>FAQ item gewijzigd</strong>',
+	'LOG_FAQ_RESTORE'			=> '<strong>FAQ hersteld via een backup</strong>',
 
 	'NOT_ALLOWED_OUT_OF_DIR'	=> 'You are not allowed to edit files out of the language directory.',
 	'NO_FAQ_FILES'				=> 'No Available FAQ Files.',
